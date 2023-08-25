9298fe5b84f549c9131a95e40e86e844ff192ddd
JBWERE-coder <lutherwere@gmail.com>
2023-08-25 21:26:09 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# "hello"
+# # "hello"
+"hello".upper()
+# # "HELLO"
+# "HELLO".lower()
+# # "hello"
+# "hello".capitalize()
+# # "Hello"
+# "hello" + "world"
+# # "helloworld"
+# "hello" * 3
+# # "hellohellohello"fbvrgvrvr
